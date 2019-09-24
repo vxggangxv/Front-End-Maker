cc7e2f564c508b804154dca1c824f65ef9beb20e
vxggangxv <vxggangxv@naver.com>
2019-09-24 18:17:04 +0900

    update

---
+++ src/plugins/firebase.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+// import firebase from 'firebase/app'
+// import * as firebase from 'firebase/app'
+// import config from '../../firebaseConfig'
+
+// import 'firebase/auth'
+// import 'firebase/firestore'
+// import 'firebase/firestore-storage'
+
+// console.log('firebase plusgin loaded')
+
+// if (!firebase.apps.length) firebase.initializeApp(config)
+
+// Vue.prototype.$db = firebase.database()
+// Vue.prototype.$auth = firebase.auth()
+
+// const { TimeStamp, GeoPoint } = firebase.firestore
+
+// Vue.prototype.$firebase = firebase
+
+// console.log('firebase plusgin end')
---
+++ src/views/E404.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="inform">
+    <div class="error">404</div>
+    <div class="content">Page Not Found</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  .inform {
+    padding-top: 50px;
+    text-align: center;
+    font-family: 'Roboto', sans-serif;
+    .error {
+      font-size: 250px;
+    }
+    .content {
+      font-size: 55px;
+    }
+  }
+</style>
