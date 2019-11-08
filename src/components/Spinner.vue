16cb9719f08a2addf6b5e74c8ca0f819a64c91fb
vxggangxv <vxggangxv@naver.com>
2019-11-08 16:44:30 +0900

    new componetns/Spinner

---
+++ src/components/Spinner.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="lds-facebook" v-if="loading">
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    loading: {
+      type: Boolean,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.lds-facebook {
+  z-index: 1000;
+  display: inline-block;
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  top: 47%;
+  left: 47%;
+  &:before {
+    content: "";
+    display: block;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.15);
+  }
+}
+.lds-facebook div {
+  display: inline-block;
+  position: absolute;
+  left: 6px;
+  width: 13px;
+  background: #42b883;
+  animation: lds-facebook 1.2s cubic-bezier(0, 0.5, 0.5, 1) infinite;
+}
+.lds-facebook div:nth-child(1) {
+  left: 6px;
+  animation-delay: -0.24s;
+}
+.lds-facebook div:nth-child(2) {
+  left: 26px;
+  animation-delay: -0.12s;
+}
+.lds-facebook div:nth-child(3) {
+  left: 45px;
+  animation-delay: 0;
+}
+@keyframes lds-facebook {
+  0% {
+    top: 6px;
+    height: 51px;
+  }
+  50%,
+  100% {
+    top: 19px;
+    height: 26px;
+  }
+}
+</style>
